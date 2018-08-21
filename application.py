7b9c9a7aa25431228615c6079db69007941a0887
Pranav Singh <pranav.singh1511@gmail.com>
2018-08-21 17:59:20 +0530

    Add starter code to template files

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -15,33 +15,33 @@ dbSession = DBSession()
 @app.route('/')
 @app.route('/catalog/')
 def show_catalog():
-    return "This page will show all categories"
+    return render_template('catalog.html')
 
 
 @app.route('/catalog/<string:category_name>/')
 @app.route('/catalog/<string:category_name>/items/')
 def show_items(category_name):
-    return ("This page will show items in category %s" % (category_name))
+    return render_template('items.html', category_name=category_name)
 
 
 @app.route('/catalog/new/')
 def new_item():
-    return ("This page will add a new item")
+    return render_template('new_item.html')
 
 
 @app.route('/catalog/<string:category_name>/<string:item_name>/')
 def show_item(category_name, item_name):
-    return ("This page will show item %s in %s" % (item_name, category_name))
+    return render_template('item.html', category_name=category_name, item_name=item_name)
 
 
 @app.route('/catalog/<string:item_name>/edit/')
 def edit_item(item_name):
-    return ("This page will edit item %s" % (item_name))
+    return render_template('edit_item.html', item_name=item_name)
 
 
 @app.route('/catalog/<string:item_name>/delete/')
 def delete_item(item_name):
-    return ("This page will delete item %s" % (item_name))
+    return render_template('delete_item.html', item_name=item_name)
 
 
 if __name__ == '__main__':
